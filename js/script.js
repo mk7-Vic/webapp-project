625e00de0199c791c9c6508c036bf8a449ab2f96
mk7-Vic <victor.mwangi@strathmore.edu>
2026-06-19 13:02:03 +0300

    Create script.js, added dynamic behaviour scroll on navbar

---
+++ js/script.js
@@ -0,0 +1,10 @@
+window.addEventListener("scroll", function () {
+    const navbar = document.querySelector(".navbar");
+
+    if (window.scrollY > 100) {
+        navbar.classList.add("shadow");
+    } else {
+        navbar.classList.remove("shadow");
+    }
+});
+
