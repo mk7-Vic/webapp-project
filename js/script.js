1affaba00f672ce84d3bb6101cfb5dfedd0a6025
mk7-Vic <victor.mwangi@strathmore.edu>
2026-06-19 23:29:50 +0300

    added a javascript counter animation to the statistics section

--- js/script.js
+++ js/script.js
@@ -20,3 +20,22 @@ pickup.addEventListener("change", function () {
     returnDate.min = pickup.value;
 });
 
+const counters = document.querySelectorAll('.counter');
+counters.forEach(counter => {
+    const updateCounter = () => {
+        const target = +counter.getAttribute('data-target');
+        const count = +counter.innerText;
+
+        const increment = target / 100;
+
+        if(count<target) {
+            counter.innerText=
+                Math.ceil(count + increment);
+                setTimeout(updateCounter,20);
+            }
+        else{
+            counter.innerText=target + '+';
+            }
+    };
+    updateCounter();
+})
